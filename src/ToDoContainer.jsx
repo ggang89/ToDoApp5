004f251f967e76bc9c3a986a2062a4c73f2485ab
ggang89 <ggang89@gmail.com>
2024-05-18 20:31:17 +0900

    todo state만들어서 상태값 연결

--- src/ToDoContainer.jsx
+++ src/ToDoContainer.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function ToDoContainer() {
+  const [todo, setTodo] = useState("투두앱만들기");
+  const [isEdting, setEdting] = useState(false);
   return (
     <>
       <label htmlFor="addTodo">할 일을 추가하세요.</label>
@@ -6,7 +10,18 @@ export default function ToDoContainer() {
       <button>추가</button>
 
       <ul>
-        <li></li>
+        {isEdting ? (
+          <>
+          <input>{todo}</input>
+          <button>저장</button>
+          </>
+        ) : (
+          <li>
+            {todo}
+            <button>수정</button>
+            <button>삭제</button>
+          </li>
+        )}
       </ul>
     </>
   );
