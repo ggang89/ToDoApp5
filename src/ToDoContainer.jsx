b6390f42c4c760f4f663aaf2c540c018c59ca2fc
ggang89 <ggang89@gmail.com>
2024-05-21 17:38:47 +0900

    TodoList를 map함수를 이용해 렌더링해줌

--- src/ToDoContainer.jsx
+++ src/ToDoContainer.jsx
@@ -1,45 +1,70 @@
 import { useState } from "react";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 export default function ToDoContainer() {
   const [addTodo, setAddTodo] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([
+    {
+      id: uuidv4(),
+      todoTitle: addTodo,
+      isEdting: false,
+    },
+  ]);
   const [isEdting, setEdting] = useState(false);
 
-const addText=()=>{
-const newArr={
-  id:uuidv4(),
-  todoTitle:addTodo,
-  isEdting:false,
-};
-setTodoList([newArr,...todoList]);
-setAddTodo(" ");
-};
-const handleAddText=(e)=>{
-setAddTodo(e.target.value);
-};
+  const addText = () => {
+    const newArr = {
+      id: uuidv4(),
+      todoTitle: addTodo,
+      isEdting: false,
+    };
+    setTodoList([newArr, ...todoList]);
+    setAddTodo(" ");
+  };
+  const handleAddText = (e) => {
+    setAddTodo(e.target.value);
+  };
 
-  const addTodoText = () => {};
-  const addBtn = () => {};
+  const handleText = (e) => {};
+  const edit = () => {};
+  const del = () => {};
   return (
     <>
       <label htmlFor="addTodo">할 일을 추가하세요.</label>
-      <input value={addTodo} onChange={handleAddText} type="text" id="addTodo" placeholder="텍스트를 입력하세요"></input>
+      <input
+        value={addTodo}
+        onChange={handleAddText}
+        type="text"
+        id="addTodo"
+        placeholder="텍스트를 입력하세요"
+      ></input>
       <button onClick={addText}>추가</button>
 
       <ul>
         {isEdting ? (
           <>
-            <input onChange={addTodoText} type="text"></input>
+            <input onChange={handleText} type="text"></input>
             {/* input 안에 값을 넣어주면 오류가 생긴다 => value값으로 넣어줘서 해결함*/}
-            <button onClick={addBtn}>저장</button>
+            <button onClick={edit}>저장</button>
           </>
         ) : (
-          <li>
-            <p></p>
-            <button>수정</button>
-            <button>삭제</button>
-          </li>
+          //todoList앞에 {}넣으면 왜 오류뜨나???
+          //<>로 감싸주거나, {}없애면 오류 안뜸
+          <>
+            {todoList.map((t) => (
+              <li key={t.id}>
+                {t.todoTitle}
+                {t.isEdting}
+                <button onClick={edit}>수정</button>
+                <button onClick={del}>삭제</button>
+              </li>
+            ))}
+          </>
+          // <li>
+          //   {todoList.map(t=>(<p key={t.id}>{t.todoTitle} {t.isEdting}</p>))}
+          //   <button onClick={edit}>수정</button>
+          //   <button onClick={del}>삭제</button>
+          // </li>
         )}
       </ul>
     </>
