e49d4b9439ca6b5111989a1cdf849a111cf82aa4
ggang89 <ggang89@gmail.com>
2024-05-21 16:38:56 +0900

    input창에 새로운 todo연결

--- src/ToDoContainer.jsx
+++ src/ToDoContainer.jsx
@@ -1,23 +1,42 @@
 import { useState } from "react";
+import {v4 as uuidv4} from "uuid";
 
 export default function ToDoContainer() {
-  const [todo, setTodo] = useState("투두앱만들기");
+  const [addTodo, setAddTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
   const [isEdting, setEdting] = useState(false);
+
+const addText=()=>{
+const newArr={
+  id:uuidv4(),
+  todoTitle:addTodo,
+  isEdting:false,
+};
+setTodoList([newArr,...todoList]);
+setAddTodo(" ");
+};
+const handleAddText=(e)=>{
+setAddTodo(e.target.value);
+};
+
+  const addTodoText = () => {};
+  const addBtn = () => {};
   return (
     <>
       <label htmlFor="addTodo">할 일을 추가하세요.</label>
-      <input type="text" id="addTodo" placeholder="텍스트를 입력하세요"></input>
-      <button>추가</button>
+      <input value={addTodo} onChange={handleAddText} type="text" id="addTodo" placeholder="텍스트를 입력하세요"></input>
+      <button onClick={addText}>추가</button>
 
       <ul>
         {isEdting ? (
           <>
-          <input>{todo}</input>
-          <button>저장</button>
+            <input onChange={addTodoText} type="text"></input>
+            {/* input 안에 값을 넣어주면 오류가 생긴다 => value값으로 넣어줘서 해결함*/}
+            <button onClick={addBtn}>저장</button>
           </>
         ) : (
           <li>
-            {todo}
+            <p></p>
             <button>수정</button>
             <button>삭제</button>
           </li>
